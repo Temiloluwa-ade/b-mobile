decf57a079e4e9cd76cd0a7d334cb4dc54b9506e
Temiloluwa Adelowo <moboluwaji003@gmail.com>
2023-08-17 21:08:55 +0100

    update: signin screen

--- Navigation/AuthStack.js
+++ Navigation/AuthStack.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "../screens/Onboarding/SplashScreen";
 import Onboard from "../screens/Onboarding/Onboard";
+import Login from "../screens/Onboarding/Login"
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,11 @@ const AuthStack = () => {
         name="Onboarding"
         component={Onboard}
         options={{ headerShown: false }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
       ></Stack.Screen>
     </Stack.Navigator>
   );
---
+++ screens/Onboarding/Login.js
@@ -0,0 +1,77 @@
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Pressable,
+  TextInput,
+  Image,
+} from "react-native";
+import React, { useState, useRef, useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const Login = () => {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  return (
+    <SafeAreaView>
+      <View className="flex justify-between flex-col">
+        <View className="mb-10">
+          <Text className="text-[#163300] font-bold text-xl self-center mt-10">
+            Sign in to resume
+          </Text>
+          <Text className="text-[#163300] font-semibold self-center">
+            your Breew experience
+          </Text>
+        </View>
+        <View>
+          <View className="mt-10 mx-6">
+            <View>
+              <Text className="text-sm">
+                Email address <Text className="text-[#FFC727]"> *</Text>
+              </Text>
+              <TextInput
+                placeholder="example@gmail.com"
+                className="mt-2 border border-[#E3E3E3] py-4 px-4 rounded-md"
+              />
+            </View>
+            <View className="mt-6">
+              <Text className="text-sm">
+                Password <Text className="text-[#FFC727]"> *</Text>
+              </Text>
+              <TextInput
+                placeholder="enter a password"
+                className="mt-2 border border-[#E3E3E3] py-4 px-4 rounded-md"
+              />
+            </View>
+          </View>
+        </View>
+        <View className="mt-10">
+          <Pressable
+            className="flex items-center justify-center text-center rounded-md"
+            onPress={() => navigation.navigate("Onboarding")}
+          >
+            <View className=" bg-[#163300] rounded-md items-center w-[90%] mx-2 p-3">
+              <Text className="text-sm text-white">Sign in</Text>
+            </View>
+          </Pressable>
+          <Pressable
+            onPress={() => navigation.navigate("Onboarding")}
+            className="mt-4"
+          >
+            <View>
+              <Text className="self-center">
+                New to Breew?
+                <Text className="font-semibold text-[#163300]">Sign up</Text>
+              </Text>
+            </View>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
